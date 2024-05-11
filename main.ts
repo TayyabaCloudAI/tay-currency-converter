0e8f756b5a110a51d5cd25ccf839b42a5eef4d49
Tayyaba <tayyabasajid.ai@gmail.com>
2024-05-11 17:53:30 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let currency:any = {
+    USD: 1,
+    Pound: 0.80,
+    Euro: 0.93,
+    INR: 83.55,
+    PKR: 278.04,
+    SAR: 3.75
+};
+
+let user_input = await inquirer.prompt ([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "Pound", "Euro", "SAR", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "Pound", "Euro", "INR", "PKR", "SAR"]
+    },
+    {
+        name: "amount",
+        message: "Enter the Amount",
+        type: "input"
+    }
+]);
+
+let userfrom_currency = user_input.from;
+let userto_currency = user_input.to;
+let from_amount = currency[userfrom_currency];
+let to_amount = currency[userto_currency];
+let amount = user_input.amount;
+
+let base_amount = amount/from_amount;
+let answer = base_amount*to_amount;
+
+console.log(answer);
+
